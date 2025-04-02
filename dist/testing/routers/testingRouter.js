0565c2c6dfbc1f71c87326df618d311c65a1fc87
kadetigor <kadet.igor3216@gmail.com>
2025-04-02 13:01:29 +0200

    First version commit

--- dist/testing/routers/testingRouter.js
+++ dist/testing/routers/testingRouter.js
@@ -4,10 +4,9 @@ exports.testingRouter = void 0;
 const express_1 = require("express");
 const db_1 = require("../../db/db");
 const httpStatus_1 = require("../../core/types/httpStatus");
-const super_admin_guard_middleware_1 = require("../../auth/middlewares/super-admin.guard-middleware");
 exports.testingRouter = (0, express_1.Router)({});
 exports.testingRouter
-    .delete('/all-data', super_admin_guard_middleware_1.superAdminGuardMiddleware, (req, res) => {
+    .delete('/all-data', (req, res) => {
     db_1.db.posts = [];
     db_1.db.blogs = [];
     res.sendStatus(httpStatus_1.HttpStatus.NoContent);
