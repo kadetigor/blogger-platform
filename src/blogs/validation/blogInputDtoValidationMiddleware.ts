181730994e4406bae05959eeb0a9d3dc884e2789
kadetigor <kadet.igor3216@gmail.com>
2025-04-02 15:38:22 +0200

    First version commit

--- src/blogs/validation/blogInputDtoValidationMiddleware.ts
+++ src/blogs/validation/blogInputDtoValidationMiddleware.ts
@@ -3,17 +3,17 @@ import { body } from 'express-validator';
 export const nameValidationMiddleware = body('name')
     .exists().withMessage('Name is required')
     .isString().withMessage('Name must be a string')
-    .trim().isLength({ max: 15 }).withMessage('Name must not be more than 15 characters long');
+    .trim().isLength({ min: 1, max: 15 }).withMessage('Name must not be more than 15 characters long');
 
 export const descriptionValidationMiddleware = body('description')
     .exists().withMessage('Description is required')
     .isString().withMessage('Description must be a string')
-    .isLength({ max: 500 }).withMessage('Description must not be more than 500 characters long');
+    .isLength({ min: 1, max: 500 }).withMessage('Description must not be more than 500 characters long');
 
 export const websiteUrlValidationMiddleware = body('websiteUrl')
     .exists().withMessage('Website URL is required')
     .isString().withMessage('Website URL must be a string')
-    .trim().isLength({ max: 100 }).withMessage('Website URL must not be more than 100 characters long')
+    .trim().isLength({ min: 1, max: 100 }).withMessage('Website URL must not be more than 100 characters long')
     .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).withMessage('Website URL must be a valid URL');
 
 export const blogInputDtoValidation = [
--- src/posts/validation/postInputDtoValidationMiddleware.ts
+++ src/posts/validation/postInputDtoValidationMiddleware.ts
@@ -2,16 +2,19 @@ import { body } from 'express-validator';
 import { blogsRepository } from '../../blogs/repositories/blogsRepository';
 
 const titleValidation = body('title')
+    .exists().withMessage('Title is required')
     .isString().withMessage('title should be string')
-    .trim().isLength({ max: 30 }).withMessage('Length of title is more than 30 characters');
+    .trim().isLength({ min:1, max: 30 }).withMessage('Length of title is more than 30 characters');
 
 const shortDescriptionValidation = body('shortDescription')
+    .exists().withMessage('shortDescription is required')
     .isString().withMessage('phoneNumber should be string')
-    .trim().isLength({ max: 100 }).withMessage('shortDescription is too long');
+    .trim().isLength({ min:1, max: 100 }).withMessage('shortDescription is too long');
 
 const contentValidation = body('content')
+    .exists().withMessage('Content is required')
     .isString().withMessage('content should be string')
-    .trim().isLength({ max: 1000 }).withMessage('Content is too long');
+    .trim().isLength({ min:1, max: 1000 }).withMessage('Content is too long');
 
 const blogIdValidation = body('blogId')
     .isString().withMessage('blogId should be string')
