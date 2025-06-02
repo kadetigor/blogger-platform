2af4d254b0c227e45e1079f5a7698fd93d0a3584
Igor Kadet <kadet.igor3216@gmail.com>
2025-06-02 19:42:10 +0200

    changed loginoremail validation to login validation logic

--- dist/users/routers/middleware/login.or.email.validation.js
+++ dist/users/routers/middleware/login.or.email.validation.js
@@ -1,9 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.loginOrEmailValidation = void 0;
 const express_validator_1 = require("express-validator");
+const mongoDb_1 = require("../../../db/mongoDb");
 exports.loginOrEmailValidation = (0, express_validator_1.body)("loginOrEmail")
-    .isString()
-    .trim()
-    .isLength({ min: 1, max: 500 })
-    .withMessage("loginOrEmail is not correct");
+    .exists().withMessage('Login is required')
+    .isString().withMessage('Login should be a string')
+    .trim().isLength({ min: 3, max: 10 }).withMessage('Length of the Login should be no less then 3 characters and no more then 10 characters')
+    .matches(/^[a-zA-Z0-9_-]*$/).withMessage('Login must contain only characters and numbers')
+    .custom((login) => __awaiter(void 0, void 0, void 0, function* () {
+    // replace `findOne` with whatever your ORM/method is
+    const existing = yield mongoDb_1.userCollection.findOne({ login });
+    if (existing) {
+        // throw to signal a failed validation
+        throw new Error('This login is already taken');
+    }
+    return true;
+}));
--- src/users/routers/middleware/login.or.email.validation.ts
+++ src/users/routers/middleware/login.or.email.validation.ts
@@ -1,7 +1,17 @@
 import {body} from "express-validator";
+import { userCollection } from "../../../db/mongoDb";
 
 export const loginOrEmailValidation = body("loginOrEmail")
-    .isString()
-    .trim()
-    .isLength({min: 1, max: 500})
-    .withMessage("loginOrEmail is not correct");
+    .exists().withMessage('Login is required')
+    .isString().withMessage('Login should be a string')
+    .trim().isLength({ min: 3, max: 10}).withMessage('Length of the Login should be no less then 3 characters and no more then 10 characters')
+    .matches(/^[a-zA-Z0-9_-]*$/).withMessage('Login must contain only characters and numbers')
+    .custom(async (login) => {
+    // replace `findOne` with whatever your ORM/method is
+        const existing = await userCollection.findOne({ login });
+        if (existing) {
+        // throw to signal a failed validation
+        throw new Error('This login is already taken');
+        }
+        return true;
+    });
